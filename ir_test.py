538b0d650fb6e257c46797ce65f6f82a833685cd
Sheng Hao Ong <shenghaoong@gmail.com>
2025-02-14 00:25:31 +0800

    Added uip_alpha

--- ir_test.py
+++ ir_test.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 from Data_Processing.ir_processing import get_ir_df
 from Alpha_Gen.uip_alpha_gen import uip_alpha_gen
+from Asset_Alloc.wq_allocator import wq_allocator
 ir_df = get_ir_df()
-ir_alpha_df = uip_alpha_gen(ir_df)
-print(ir_alpha_df.head())
+ir_alpha_df = uip_alpha_gen(ir_df) - 1
+ir_alloc = wq_allocator(ir_alpha_df)
+print(ir_alloc.head())
